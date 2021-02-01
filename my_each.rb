6eda14fc90b723de136b7a8b2e298b0e4eacb826
Sam D <samidiaz3@gmail.com>
2021-02-01 22:47:40 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -5,11 +5,12 @@ def my_each (array)# put argument(s) here
   new_array = []
   # while loop to iterate. yiled each eleement containted in array to block
   i = 0
-
+  n = 0
   while i < array.length
-    yield (array[0])
-    new_array << array[0]
+    yield (array[n])
+    new_array << array[n]
     i = i + 1
+    n = n + 1
   end
   new_array
 end
