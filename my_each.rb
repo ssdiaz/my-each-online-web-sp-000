c020279b8ad9dced40ecae1fbcf9e7ca44abe54e
Sam D <samidiaz3@gmail.com>
2021-02-01 22:27:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,10 @@
 def my_each (array)# put argument(s) here
   # code here
-
+  new_array = []
   # while loop to iterate. yiled each eleement containted in array to block
   while i < array.length
     puts i
+    yield
   end
 
 end
