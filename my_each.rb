01b9c2c917468d30c8186cff4d744ad993a25769
Sam D <samidiaz3@gmail.com>
2021-02-01 22:51:04 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,12 +4,12 @@ def my_each (array)# put argument(s) here
   # code here
   new_array = []
   # while loop to iterate. yiled each eleement containted in array to block
-  counter = 0
+  #counter = 0
   index = 0
-  while counter < array.length
+  while index < array.length
     yield (array[index])
     new_array << array[index]
-    counter = counter + 1
+    #counter = counter + 1
     index = index + 1
   end
   new_array
